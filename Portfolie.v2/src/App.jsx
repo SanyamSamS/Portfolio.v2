7a4f871ee3252bf27a942fd8afb545e60e577929
Sam Gill <samgill@sams-mbp.home>
2024-03-12 22:24:00 -0400

    Added placeholder content for components

--- Portfolie.v2/src/App.jsx
+++ Portfolie.v2/src/App.jsx
@@ -3,20 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { NavBar } from './components/NavBar/NavBar'
 import { Home } from './components/Home/Home'
-import { Banner } from './components/Banner/Banner'
 import { About } from './components/About/About'
 import { Projects } from './components/Projects/Projects'
 import { Contact } from './components/Contact/Contact'
+import { Banner } from './components/Banner/Banner'
+
 
 function App() {
   return (
     < >
       <NavBar />
       <Home />
-      <Banner />
       <About />
       <Projects />
       <Contact />
+      <Banner />
     </>
   )
 }
--- Portfolie.v2/src/components/Banner/Banner.jsx
+++ Portfolie.v2/src/components/Banner/Banner.jsx
@@ -1,14 +1,7 @@
-import { Container, Row } from "react-bootstrap"
-
 export const Banner = () => {  
     return (
         <div>
-            <Container>
-                <Row>
-                    <Col> 
-                        <h2>Hi, I'm <span>Sanyam</span></h2>
-                    </Col>
-            </Container>
+            <h2>Hi, I'm <span>Sanyam</span></h2>
         </div>
     )
 }
