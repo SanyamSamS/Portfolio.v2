2d2d0f1c6170ab7d7741b2d8a622b6d29c73b3d9
Sam Gill <samgill@sams-mbp.home>
2024-03-13 19:15:30 -0400

    changed navbar, a tag, and image styling

--- Portfolie.v2/src/components/Home/Home.jsx
+++ Portfolie.v2/src/components/Home/Home.jsx
@@ -1,14 +1,16 @@
 import '../../App.css';
 import './Home.css';
+import meLaptopImg from '../../assets/img/me-laptop.png';
 
 export const Home = () => {   
     return (
         <section id="home" className="home-section">
           <div className="container">
+            <img src={meLaptopImg} alt="Me with Laptop" className="home-img" />
             <div className="intro-text">
               <h1>Hi I'm Sanyam</h1>
               <p>Full Stack Web Developer.</p>
-              <a href="#about" className="btn btn-primary">Learn More About Me</a>
+              <a href="#about" className="btn btn-primary">Learn More</a>
             </div>
           </div>
         </section>
