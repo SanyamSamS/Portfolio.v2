a03526f3f01be4d4fcac1fac3a735a3faba516dd
Sam Gill <samgill@sams-mbp.home>
2024-03-19 23:25:29 -0400

    minor style update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,7 +14,7 @@ export const About = () => {
           <div className="col-lg-4 col-md-4 col-sm-12">
             <div className="info-box">
               <h3>Me</h3>
-              <p>Me</p>
+              <p>I'm Sam, a Full Stack Web Developer, graduate from the University of Toronto's coding bootcamp. My journey into web development has been fueled by a passion for solving complex problems and creating user-friendly interfaces. I bring enthusiasm, a keen willingness to learn, and a commitment to developing high-quality web solutions. I'm excited to collaborate and grow alongside experienced teams, contributing fresh perspectives and innovative ideas. </p>
             </div>
           </div>
 
@@ -25,7 +25,7 @@ export const About = () => {
               <ul className='skill-list'>
                 <li><i className="fa fa-html5"></i> HTML</li>
                 <li><i className="fa fa-css3"></i> CSS</li>
-                <li><img src={jsIcon} /> JavaScript</li>
+                <li><i className="material-symbols-outlined">javascript</i> JavaScript</li>
                 <li><i className="fa-brands fa-react"></i> React.js</li>
                 <li><i className="fa fa-database"></i> MongoDB, SQL</li>
                 <li><i className="fa fa-node-js"></i> Node.js, Express.js</li>
